eefd1c19eabff486e1cb0e7c40737d58f4eb45c1
pick3t <maruzw0416@gmail.com>
2023-01-02 20:49:50 +0800

    feat: Helper class map_init_helper is created for explicit declaration of map.

---
+++ src/tools/enum_str_map/enum_str_map.cpp
@@ -0,0 +1,3 @@
+#include "enum_str_map.hpp"
+
+using namespace CommonTools;
---
+++ src/tools/enum_str_map/enum_str_map.hpp
@@ -0,0 +1,26 @@
+namespace CommonTools {
+/* T needs to be a map type */
+template<typename MapType>
+struct map_init_helper
+{
+    MapType& map;
+    map_init_helper(MapType& m) : map(m) {}
+    map_init_helper& operator()(typename MapType::key_type const& key, typename MapType::mapped_type const& value)
+    {
+        map[key] = value;
+        return *this;
+    }
+
+    typename MapType::mapped_type const& operator[](typename MapType::key_type const& key)
+    {
+        return map[key];
+    }
+};
+
+template<typename MapType>
+constexpr map_init_helper<MapType> map_init(MapType& item)
+{
+    return map_init_helper<MapType>(item);
+}
+
+}
